8d3b666096eee688a9ffe06f5eaf3f480495b2f5
Shivaniambati006 <157499537+Shivaniambati006@users.noreply.github.com>
2026-06-14 21:46:56 +0530

    Commit

---
+++ docs/html/dir_d44c64559bbebec7f509842c48db8b23.js
@@ -0,0 +1,5 @@
+var dir_d44c64559bbebec7f509842c48db8b23 =
+[
+    [ "mathlib.h", "mathlib_8h_source.html", null ],
+    [ "platform.h", "platform_8h_source.html", null ]
+];
---
+++ docs/html/navtreeindex0.js
@@ -0,0 +1,9 @@
+var NAVTREEINDEX0 =
+{
+"dir_d44c64559bbebec7f509842c48db8b23.html":[0,0,0],
+"files.html":[0,0],
+"index.html":[],
+"mathlib_8h_source.html":[0,0,0,0],
+"pages.html":[],
+"platform_8h_source.html":[0,0,0,1]
+};
